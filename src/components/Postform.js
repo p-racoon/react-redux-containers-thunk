90f5ee811d1fce35d6228f25b45601efa9a99196
p-racoon <prasham.ashesh>
2019-02-19 15:14:27 +0530

    added the ugly form

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -9,6 +9,23 @@ class Postform extends Component {
     return (
       <div>
         <h2>Add Post</h2>
+        <form>
+            <div>
+                <label>Title:</label>
+                <br/>
+                <input type="text" name="title"/>
+            </div>
+            <br/>
+            <div>
+                <label>Body:</label>
+                <br/>
+                <textarea  name="body"/>
+            </div>
+            <br/>
+            <div>
+                <button type="submit" >Submit</button>
+            </div>
+        </form>
       </div>
     )
   }
