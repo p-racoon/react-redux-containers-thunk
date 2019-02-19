74e8d51692a724de330f49199aa80a07907ca912
p-racoon <prasham.ashesh>
2019-02-19 15:47:35 +0530

    add to the posts and print hte returned post in console

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,34 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Postform extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={};
+        this.state = {
+            title: '',
+            body: '',
+        };
+        this.handleOnChange = this.handleOnChange.bind(this);//Shift+Alt+Down Arrow Key to copy the line down
+        this.handleOnSubmit = this.handleOnSubmit.bind(this);
     }
-  render() {
-    return (
-      <div>
-        <h2>Add Post</h2>
-        <form>
+    handleOnChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    handleOnSubmit(e) {
+        e.preventDefault();//this will prevent the submit button from reloading the page
+        const newPost = {
+            title: this.state.title,
+            body: this.state.body
+        }
+        //this doesnt add to the dataqbase but it will give us the response back 
+        //with the added post as if it were added to the database
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newPost)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+    }
+    render() {
+        return (
             <div>
-                <label>Title:</label>
-                <br/>
-                <input type="text" name="title"/>
+                <h2>Add Post</h2>
+                <form onSubmit={this.handleOnSubmit}>
+                    <div>
+                        <label>Title:</label>
+                        <br />
+                        <input type="text" name="title" value={this.state.title} onChange={this.handleOnChange} />
+                    </div>
+                    <br />
+                    <div>
+                        <label>Body:</label>
+                        <br />
+                        <textarea name="body" value={this.state.body} onChange={this.handleOnChange} />
+                    </div>
+                    <br />
+                    <div>
+                        <button type="submit" >Submit</button>
+                    </div>
+                </form>
             </div>
-            <br/>
-            <div>
-                <label>Body:</label>
-                <br/>
-                <textarea  name="body"/>
-            </div>
-            <br/>
-            <div>
-                <button type="submit" >Submit</button>
-            </div>
-        </form>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default Postform;
